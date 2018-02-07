c4521060c4e91e9b27cba226ccd86226246011c7
Cecilia Wong <cissi.wong@gmail.com>
2018-02-07 14:07:50 +0100

    style salonview

--- code/src/components/listview.js
+++ code/src/components/listview.js
@@ -16,4 +16,4 @@ export default class ListView extends React.Component {
       </div>
     )
   }
- }
+}
--- code/src/components/salonview.js
+++ code/src/components/salonview.js
@@ -1,6 +1,14 @@
 import React from "react"
-// import data from "./data.json"
-// import Salon from "./salon.js"
+import header from "../assets/header_img.png"
+import statusbar from "../assets/status_bar.png"
+import heart from "../assets/heart.png"
+import left from "../assets/left.png"
+import phone from "../assets/phone.png"
+import star from "../assets/star.png"
+import staroutline from "../assets/star-outline.png"
+import location from "../assets/location.png"
+import web from "../assets/web.png"
+import line from "../assets/line.png"
 
 const data = require("./data.json")
 
@@ -27,8 +35,47 @@ export default class SalonView extends React.Component {
 
   render() {
     return (
-      <div className="listview-container">
-      <h1>{this.state.title}</h1>
+      <div className="salonview">
+        <header className="salonview-header">
+          <img className="statusbar" src={statusbar} alt="" />
+          <img className="header_img" src={header} alt="" />
+          <div>
+            <img className="left" src={left} alt="" />
+          </div>
+          <img className="heart" src={heart} alt="" />
+          <h1>{this.state.salon.title}</h1>
+          <div className="rating">
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={staroutline} alt="" />
+            <p>(32)</p>
+          </div>
+        </header>
+        <nav>
+          <h2>Info</h2>
+          <h2>Schema</h2>
+        </nav>
+        <img className="salonview-line" src={line} alt="" />
+        <div className="salonview-container">
+          <div className="salonview-address">
+            <img src={location} alt="" />
+            <p>{this.state.salon.address}</p>
+          </div>
+          <div className="salonview-time">
+            <p>12.00</p>
+          </div>
+          <div className="salonview-phone">
+            <img src={phone} alt="" />
+            <p>{this.state.salon.phone}</p>
+          </div>
+          <div className="salonview-web">
+            <img src={web} alt="" />
+            <p>{this.state.salon.website}</p>
+          </div>
+          <p>{this.state.salon.text}</p>
+        </div>
       </div>
     )
   }
