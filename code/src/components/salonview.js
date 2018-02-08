d27b8791c82fd5ca82c1c842c68754d2fc71f177
Cecilia Wong <cissi.wong@gmail.com>
2018-02-08 09:23:07 +0100

    added home link

--- code/src/components/salonview.js
+++ code/src/components/salonview.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import header from "../assets/header_img.png"
 import statusbar from "../assets/status_bar.png"
 import heart from "../assets/heart.png"
@@ -9,6 +10,7 @@ import staroutline from "../assets/star-outline.png"
 import location from "../assets/location.png"
 import web from "../assets/web.png"
 import line from "../assets/line.png"
+import down from "../assets/down.png"
 
 const data = require("./data.json")
 
@@ -16,7 +18,8 @@ export default class SalonView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      salon: {}
+      salon: {},
+      home: false
     }
   }
 
@@ -33,14 +36,16 @@ export default class SalonView extends React.Component {
     })
   }
 
+
+
   render() {
     return (
       <div className="salonview">
         <header className="salonview-header">
           <img className="statusbar" src={statusbar} alt="" />
           <img className="header_img" src={header} alt="" />
-          <div>
-            <img className="left" src={left} alt="" />
+          <div className="left">
+            <Link to="/"><img src={left} alt="" /></Link>
           </div>
           <img className="heart" src={heart} alt="" />
           <h1>{this.state.salon.title}</h1>
@@ -61,10 +66,12 @@ export default class SalonView extends React.Component {
         <div className="salonview-container">
           <div className="salonview-address">
             <img src={location} alt="" />
-            <p>{this.state.salon.address}</p>
+            <p>{this.state.salon.address}, {this.state.salon.postal}</p>
           </div>
           <div className="salonview-time">
-            <p>12.00</p>
+            <img className="location" src={location} alt="" />
+            <p>{this.state.salon.openinghours}</p>
+            <img className="down" src={down} alt="" />
           </div>
           <div className="salonview-phone">
             <img src={phone} alt="" />
